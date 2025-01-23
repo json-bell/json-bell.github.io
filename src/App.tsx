dc91e35a50ab145b2b70dc9e2110c253190b45e2
Jason Bell <jasonbell143@gmail.com>
2025-01-23 01:11:39 +0000

    clears default styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import BackgroundCanvas from "./components/Backgrounds/BackgroundCanvas/BackgroundCanvas";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>Hi Maria :D</p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BackgroundCanvas />
+      <p>Hi Maria :D</p>
     </>
   );
 }
